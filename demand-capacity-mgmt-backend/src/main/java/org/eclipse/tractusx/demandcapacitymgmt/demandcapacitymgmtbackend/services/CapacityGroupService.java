83e1f79c16768cf9527ee4fe92a06b6bb5220004
Pedro Bailão <p.bailao@campus.fct.unl.pt>
2023-08-11 13:00:37 +0100

    Fix: create, getAll and getbyid in CapacityGroup

--- demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/services/CapacityGroupService.java
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/services/CapacityGroupService.java
@@ -1,12 +1,19 @@
 package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.services;
 
+import eclipse.tractusx.demand_capacity_mgmt_specification.model.CapacityGroupDefaultViewResponse;
 import eclipse.tractusx.demand_capacity_mgmt_specification.model.CapacityGroupRequest;
+import eclipse.tractusx.demand_capacity_mgmt_specification.model.CapacityGroupResponse;
+import eclipse.tractusx.demand_capacity_mgmt_specification.model.MaterialDemandResponse;
 import java.util.List;
 import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.entities.CapacityGroupEntity;
 import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.entities.enums.CapacityGroupStatus;
 
 public interface CapacityGroupService {
-    void createCapacityGroup(CapacityGroupRequest capacityGroupRequest);
+    CapacityGroupResponse createCapacityGroup(CapacityGroupRequest capacityGroupRequest);
+
+    CapacityGroupResponse getCapacityGroupById(String CapacityGroupId);
 
     List<CapacityGroupEntity> getAllByStatus(CapacityGroupStatus status);
+
+    List<CapacityGroupDefaultViewResponse> getAll();
 }
