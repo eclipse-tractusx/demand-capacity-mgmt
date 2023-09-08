16488a969657737ce7535e99c811c1aaccd01cdc
Pedro Bailão <p.bailao@campus.fct.unl.pt>
2023-09-08 17:03:46 +0100

    feat: Added weeks by moths on chronogram

--- demand-capacity-mgmt-frontend/src/components/capacitygroup/CapacityGroupDetailsPage.tsx
+++ demand-capacity-mgmt-frontend/src/components/capacitygroup/CapacityGroupDetailsPage.tsx
@@ -20,7 +20,7 @@
  *    ********************************************************************************
  */
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, ButtonGroup, Button, ToggleButton } from 'react-bootstrap';
 import CapacityGroupChronogram from "./CapacityGroupChronogram";
 
@@ -104,3 +104,31 @@ function CapacityGroupDetailsPage() {
 }
 
 export default CapacityGroupDetailsPage;
+
+
+
+const renderMonthTick = (tickProps: any) => {
+  const { x, y, payload } = tickProps;
+  const { value } = payload;
+  const date = new Date(value);
+  const monthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
+  const month = date.getMonth();
+
+
+  // For beginning and end of the month
+  if (date.getDate() === 1 || new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() === date.getDate()) {
+    const pathX = Math.floor(x) + 0.5;
+    return (
+        <g>
+          <path d={`M${pathX},${y - 4}v${-35}`} stroke="red" />
+          <text x={x} y={y - 40} textAnchor="middle">{monthNames[month]}</text>
+        </g>
+    );
+  }
+
+  return <g />; // Return empty SVG group element instead of null
+};
