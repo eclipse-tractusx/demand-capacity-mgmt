f2266d4ad607d88aac9ea817a46d4d0c10d32356
Pedro Bailão <p.bailao@campus.fct.unl.pt>
2023-09-08 15:28:00 +0100

    add: capacity demand group chronogram

--- demand-capacity-mgmt-frontend/src/components/capacitygroup/CapacityGroupDetailsPage.tsx
+++ demand-capacity-mgmt-frontend/src/components/capacitygroup/CapacityGroupDetailsPage.tsx
@@ -21,11 +21,26 @@
  */
 
 import { useState } from 'react';
-import { Tab, Tabs} from 'react-bootstrap';
-
+import { Tab, Tabs, ButtonGroup, Button, ToggleButton } from 'react-bootstrap';
+import CapacityGroupChronogram from "./CapacityGroupChronogram";
 
 function CapacityGroupDetailsPage() {
+  const [editMode, setEditMode] = useState(false);
+  const [savedChanges, setSavedChanges] = useState(false);
   const [activeTab, setActiveTab] = useState('overview');
+
+  const handleSave = () => {
+    // Perform save operation here
+    setEditMode(false);
+    setSavedChanges(true);
+    console.log(savedChanges);// todo clean
+  };
+
+  const handleRevert = () => {
+    // Revert changes here
+    setEditMode(false);
+    setSavedChanges(false);
+  };
   
 
   return (
@@ -39,6 +54,27 @@ function CapacityGroupDetailsPage() {
           </div>
             <div className="col d-flex justify-content-end">
               <br />
+              {activeTab === 'overview' && (
+              <ButtonGroup className="mb-2 align-middle">
+                <ToggleButton
+                  id="toggle-edit"
+                  type="checkbox"
+                  variant="secondary"
+                  name="edit"
+                  value="0"
+                  checked={editMode}
+                  onChange={() => setEditMode(!editMode)}
+                >
+                  Edit
+                </ToggleButton>
+                <Button variant="secondary" name="save" onClick={handleSave}>
+                  Save
+                </Button>
+                <Button variant="secondary" name="revert" onClick={handleRevert}>
+                  Revert Changes
+                </Button>
+              </ButtonGroup>
+                        )}
             </div>  
         </div>
         <Tabs
@@ -53,9 +89,7 @@ function CapacityGroupDetailsPage() {
           }}
         >
           <Tab eventKey="overview" title="Overview">
-
-            TABLE
-            chronogram here
+            <CapacityGroupChronogram />
           </Tab>
           <Tab eventKey="materials" title="Materials">
             Materials Table here
