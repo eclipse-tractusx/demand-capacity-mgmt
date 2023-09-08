b19348da275a9f2965b62f2abbe8503b44e85884
Pedro Bailão <p.bailao@campus.fct.unl.pt>
2023-09-08 18:03:27 +0100

    feat: added brush and some mock data to the grafic

--- demand-capacity-mgmt-frontend/src/components/capacitygroup/CapacityGroupChronogram.tsx
+++ demand-capacity-mgmt-frontend/src/components/capacitygroup/CapacityGroupChronogram.tsx
@@ -29,7 +29,7 @@ import {
     YAxis,
     CartesianGrid,
     Tooltip,
-    Legend
+    Legend, Brush
 } from "recharts";
 
 
@@ -108,8 +108,116 @@ const rawData = [
         cnt: 400
     },
     {
-        name: "Page AA",
+        name: "Page x",
         date: "2003-10-25",
+        ActualCapacity: 1550,
+        Demand: 745,
+        MaximumCapacity: 1750,
+        cnt: 405
+    },
+
+    {
+        name: "Page A",
+        date: "2004-01-06",
+        ActualCapacity: 590,
+        Demand: 800,
+        MaximumCapacity: 1400,
+        cnt: 490
+    },
+    {
+        name: "Page B",
+        date: "2004-02-06",
+        ActualCapacity: 868,
+        Demand: 967,
+        MaximumCapacity: 1506,
+        cnt: 590
+    },
+    {
+        name: "Page C",
+        date:"2004-03-06",
+        ActualCapacity: 1397,
+        Demand: 1098,
+        MaximumCapacity: 989,
+        cnt: 350
+    },
+    {
+        name: "Page D",
+        date: "2004-04-22",
+        ActualCapacity: 1480,
+        Demand: 1200,
+        MaximumCapacity: 1228,
+        cnt: 480
+    },
+    {
+        name: "Page E",
+        date: "2004-05-20",
+        ActualCapacity: 1520,
+        Demand: 1108,
+        MaximumCapacity: 1100,
+        cnt: 460
+    },
+    {
+        name: "Page F",
+        date: "2004-06-04",
+        ActualCapacity: 1400,
+        Demand: 680,
+        MaximumCapacity: 1700,
+        cnt: 380
+    },
+
+    {
+        name: "Page G",
+        date: "2004-07-10",
+        ActualCapacity: 1450,
+        Demand: 705,
+        MaximumCapacity: 1650,
+        cnt: 390
+    },
+    {
+        name: "Page H",
+        date: "2004-08-15",
+        ActualCapacity: 1500,
+        Demand: 720,
+        MaximumCapacity: 1620,
+        cnt: 395
+    },
+    {
+        name: "Page Z",
+        date: "2004-09-20",
+        ActualCapacity: 1525,
+        Demand: 735,
+        MaximumCapacity: 1725,
+        cnt: 400
+    },
+    {
+        name: "Page AA",
+        date: "2023-10-25",
+        ActualCapacity: 1550,
+        Demand: 745,
+        MaximumCapacity: 1750,
+        cnt: 405
+    },
+
+    {
+        name: "Page AA",
+        date: "2004-11-25",
+        ActualCapacity: 1550,
+        Demand: 745,
+        MaximumCapacity: 1750,
+        cnt: 405
+    },
+    {
+        name: "Page AA",
+        date: "2004-12-1",
+        ActualCapacity: 1550,
+        Demand: 745,
+        MaximumCapacity: 1750,
+        cnt: 405
+    },
+
+    {
+        name: "Page AA",
+        date: "2004-12-9",
         ActualCapacity: 1550,
         Demand: 745,
         MaximumCapacity: 1750,
@@ -180,12 +288,14 @@ function CapacityGroupChronogram() {
             width={1300}
             height={500}
             data={data}
+
             margin={{
                 top: 20,
                 right: 80,
                 bottom: 20,
                 left: 20
             }}
+
         >
 
             <CartesianGrid stroke="#f5f5f5"/>
@@ -209,6 +319,7 @@ function CapacityGroupChronogram() {
             <Bar dataKey="Demand" barSize={20} fill="#413ea0"/>
             <Line type="monotone" dataKey="ActualCapacity" stroke="#ff7300"/>
             <Line type="monotone" dataKey="MaximumCapacity"  stroke="#8884d8"/>
+            <Brush y={450} dataKey="date" height={20} stroke="#8884d8" />
         </ComposedChart>
     );
 }
