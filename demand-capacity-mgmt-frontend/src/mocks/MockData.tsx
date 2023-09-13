ed2eac7aba01430e3b1a6e7f712a13891f02034c
Pedro Bailão <p.bailao@campus.fct.unl.pt>
2023-09-13 17:46:20 +0100

    fix: added header

--- demand-capacity-mgmt-frontend/src/mocks/MockData.tsx
+++ demand-capacity-mgmt-frontend/src/mocks/MockData.tsx
@@ -1,3 +1,24 @@
+/*
+ *  *******************************************************************************
+ *  Copyright (c) 2023 BMW AG
+ *  Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ *    See the NOTICE file(s) distributed with this work for additional
+ *    information regarding copyright ownership.
+ *
+ *    This program and the accompanying materials are made available under the
+ *    terms of the Apache License, Version 2.0 which is available at
+ *    https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ *
+ *    SPDX-License-Identifier: Apache-2.0
+ *    ********************************************************************************
+ */
 export const mockInfoMenuData = {
     "todos": {
         "count": 12,
@@ -14,7 +35,7 @@ export const mockInfoMenuData = {
         "materialDemandsIds": ["423432434", "324343443", "4324324324"],
         "capacityGroups": ["423432434", "4324324324", "4324324324"]
     },
-    "statusDegredation": {
+    "statusDegradation": {
         "count": 15,
         "materialDemandsIds": ["43434434", "4324324324", "4324324324"],
         "capacityGroups": ["423432434", "4324324324", "4324324324"]
