29eda5e4ac9a8351a213dbbcfb6abe45879be8a8
Diogo Sousa <diogo12246@gmail.com>
2023-09-19 17:20:00 +0100

    push conflicts

--- demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/security/SecurityConfig.java
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/security/SecurityConfig.java
@@ -18,17 +18,18 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(
-            authorize ->
-                authorize
-                    .requestMatchers(HttpMethod.POST, "/token/login", "/token/refresh", "/token/logout", "/token/introspect")
-                    .permitAll()
-                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
+                authorize ->
+                        authorize
+                                .requestMatchers(HttpMethod.POST, "/token/login", "/token/refresh", "/token/logout", "/token/introspect")
+                                .permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
         );
         http.oauth2ResourceServer(t -> t.jwt(Customizer.withDefaults()));
         http.sessionManagement(t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+
     }
 }
