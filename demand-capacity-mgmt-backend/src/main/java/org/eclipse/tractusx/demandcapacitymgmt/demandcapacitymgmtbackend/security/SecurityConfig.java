4c6c6804bb773f5cc446d8added6a7fefeeb20b2
Diogo Sousa <diogo12246@gmail.com>
2023-09-19 11:08:46 +0100

    backend keycloak secured
    implemented token free GET endpoint that begins with /public
    implemented all endpoints to be secured with keycloak using OAUTH2 and Spring Boot 3

---
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorize ->
+            authorize
+            .requestMatchers(HttpMethod.GET,"/public").permitAll()
+            .anyRequest().authenticated()
+        );
+        http.oauth2ResourceServer(t -> t.jwt(Customizer.withDefaults()));
+        http.sessionManagement(t->t.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
