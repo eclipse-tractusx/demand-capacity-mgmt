c2f65f4c166c6de84c6219d49cd15ac2a7d0dbb5
Saja Adem <sajaadem@gmail.com>
2023-10-05 16:28:59 +0200

    reuse refactored CapacityGroupServiceImpl file

--- demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/services/impl/FavoriteServiceImpl.java
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/services/impl/FavoriteServiceImpl.java
@@ -76,13 +76,11 @@ public class FavoriteServiceImpl implements FavoriteService {
             entity.setType(FavoriteType.valueOf(favoriteRequest.getfType()));
             favoriteRepository.saveAndFlush(entity);
             return convertFavoriteResponse(entity);
-
         } else throw new NotFoundException(
             404,
             "Demand category not found",
             new ArrayList<>(List.of("provided UUID did not match any records. - " + id))
         );
-
     }
 
     @Override
