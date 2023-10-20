719fbd71b8cfd12a5f07317aad0c0bb882b4d29e
Sergio Figueiredo <informatic5.0@gmail.com>
2023-10-20 18:51:07 +0100

    TopMenu Auto Refresh

--- demand-capacity-mgmt-frontend/src/components/dcm/Layout.tsx
+++ demand-capacity-mgmt-frontend/src/components/dcm/Layout.tsx
@@ -1,4 +1,7 @@
 import React, { ReactNode } from 'react';
+import CapacityGroupsProvider from '../../contexts/CapacityGroupsContextProvider';
+import DemandCategoryContextProvider from '../../contexts/DemandCategoryProvider';
+import EventsContextProvider from '../../contexts/EventsContextProvider';
 import { InfoMenuProvider } from '../../contexts/InfoMenuContextProvider';
 import QuickAcessItems from '../common/QuickAcessItems';
 import TopMenu from '../common/TopMenu';
@@ -10,9 +13,16 @@ interface Props {
 const Layout: React.FC<Props> = ({ children }) => {
     return (
         <div className='root-container'>
-            <InfoMenuProvider>
-                <TopMenu />
-            </InfoMenuProvider>
+            <DemandCategoryContextProvider>
+                <CapacityGroupsProvider>
+                    <EventsContextProvider>
+                        <InfoMenuProvider>
+                            <TopMenu />
+                        </InfoMenuProvider>
+                    </EventsContextProvider>
+                </CapacityGroupsProvider>
+            </DemandCategoryContextProvider>
+
             <div className='overflow-control-container'>
                 {children}
                 <QuickAcessItems />
