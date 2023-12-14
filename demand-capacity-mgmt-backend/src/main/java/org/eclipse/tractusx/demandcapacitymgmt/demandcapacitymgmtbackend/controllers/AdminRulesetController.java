bdf86896c8d5f287f94f3e513df94d59e0a29107
Diogo Sousa <diogo12246@gmail.com>
2023-12-14 12:00:13 +0000

    ruleset open api spec

---
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/controllers/AdminRulesetController.java
@@ -0,0 +1,13 @@
+package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class AdminRulesetController {
+
+    private HttpServletRequest request;
+
+}
