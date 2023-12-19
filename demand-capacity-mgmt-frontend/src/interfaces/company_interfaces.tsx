bd926423a63a0cd7003085d7c17b6f9efc5143e3
Sergio Figueiredo <informatic5.0@gmail.com>
2023-12-19 17:39:53 +0000

    Modals

--- demand-capacity-mgmt-frontend/src/interfaces/company_interfaces.tsx
+++ demand-capacity-mgmt-frontend/src/interfaces/company_interfaces.tsx
@@ -14,18 +14,3 @@ export interface CompanyData {
     edc_url: string,
     isEdcRegistered: boolean,
 }
-
-export interface CompanyCreate {
-    id: string,
-    companyName: string,
-    bpn: string,
-    street: string,
-    zipCode: string,
-    country: string,
-    number: string,
-
-    bpnType: string,
-    edc_url: string,
-    isEdcRegistered: boolean,
-}
-
