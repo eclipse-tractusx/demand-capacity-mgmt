7737b90a9705a2e2db0741945dbd35bc1b0452a3
Diogo Sousa <diogo12246@gmail.com>
2024-01-05 17:17:14 +0000

    default year report Dto generation

--- demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/controllers/YearReportController.java
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/controllers/YearReportController.java
@@ -26,9 +26,14 @@ import eclipse.tractusx.demand_capacity_mgmt_specification.api.YearReportApi;
 import eclipse.tractusx.demand_capacity_mgmt_specification.model.YearReport;
 import eclipse.tractusx.demand_capacity_mgmt_specification.model.YearReportRequest;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
 import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.services.impl.BottleneckManagerImpl;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.utils.UserUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@AllArgsConstructor
 public class YearReportController implements YearReportApi {
 
     private HttpServletRequest request;
@@ -38,7 +43,7 @@ public class YearReportController implements YearReportApi {
     @Override
     public ResponseEntity<YearReport> generateYearReport(YearReportRequest yearReportRequest) throws Exception {
         return ResponseEntity.status(200).body(
-                bottleneckManager.generateYearReport(yearReportRequest.getCgID())
+                bottleneckManager.generateYearReport(UserUtil.getUserID(request),yearReportRequest.getCgID())
         );
     }
 
