b53105ab3d9a53684615ef3926619b066d91df64
Ravi Ghadiya <ravi.ghadiya@smartsensesolutions.com>
2024-05-16 12:07:03 +0530

    Managed ConfigurationPropertiesScan annotation on main class

--- demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/DemandCapacityMgmtBackendApplication.java
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/DemandCapacityMgmtBackendApplication.java
@@ -24,10 +24,12 @@ package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ConfigurationPropertiesScan
 public class DemandCapacityMgmtBackendApplication {
 
     public static void main(String[] args) {
