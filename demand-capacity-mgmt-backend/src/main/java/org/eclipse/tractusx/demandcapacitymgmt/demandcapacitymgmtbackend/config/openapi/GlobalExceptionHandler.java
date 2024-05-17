a65f34f74920d77d3ce59b3b9e9b21338f4aecd5
Nitin Vavdiya <nitin.vavdiya@smartsensesolutions.com>
2024-05-17 11:08:19 +0200

    fix: retrn http status 500 when we have internal server error

---
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/config/openapi/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.config.openapi;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+
+	//ExceptionHandler(Exception.class)
+	ProblemDetail handleException(Exception e) {
+		log.error("Error ", e);
+		String errorMsg = ExceptionUtils.getMessage(e);
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, errorMsg);
+		problemDetail.setTitle(errorMsg);
+		problemDetail.setProperty(System.currentTimeMillis()+"", System.currentTimeMillis());
+		return problemDetail;
+	}
+
+}
